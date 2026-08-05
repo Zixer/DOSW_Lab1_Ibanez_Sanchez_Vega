748254ac5c0e081ad1fe7512171ee03d97be93cc
Yazid Alejandro Sánchez Sánchez <yazid.sanchez-s@mail.escuelaing.edu.co>
2026-08-04 23:06:10 -0500

    Add divisible by 2 valid

--- Laboratory1/Challenge2/ParallelRace.java
+++ Laboratory1/Challenge2/ParallelRace.java
@@ -2,6 +2,10 @@ import java.util.List;
 
 public class ParallelRace {
 
+    public static int findMax(List<Integer> numbers) {
+        return numbers.stream().max(Integer::compare).orElse(0);
+    }
+
     public static int findMin(List<Integer> numbers) {
         return numbers.stream().min(Integer::compare).orElse(0);
     }
@@ -10,15 +14,20 @@ public class ParallelRace {
         return numbers.stream().count();
     }
 
-    public static void main(String[] args) {
-        List<Integer> sample = List.of(5, 12, 3, 20, 8);
-        System.out.println("Min value: " + findMin(sample));
-        System.out.println("Total elements: " + countElements(sample));
-    }
-
     public static Result combinada(List<Integer> numbers) {
+        int max = findMax(numbers);
         int min = findMin(numbers);
         long count = countElements(numbers);
-        return new Result(0, min, count);
+        String divisibleCheck = (max % 2 == 0) ? "Divisible by 2" : "Not divisible by 2";
+        System.out.println("Max check: " + divisibleCheck);
+        return new Result(max, min, count);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> sample = List.of(5, 12, 3, 20, 8);
+        System.out.println("Max value: " + findMax(sample));
+        System.out.println("Min value: " + findMin(sample));
+        System.out.println("Total elements: " + countElements(sample));
+        Result res = combinada(sample);
     }
 }
