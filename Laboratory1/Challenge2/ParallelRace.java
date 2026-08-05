29a2de3cb4090dc543b3f24525687bc6403b3c94
Yazid Alejandro Sánchez Sánchez <yazid.sanchez-s@mail.escuelaing.edu.co>
2026-08-04 21:55:47 -0500

    Lane Two complete

--- Laboratory1/Challenge2/ParallelRace.java
+++ Laboratory1/Challenge2/ParallelRace.java
@@ -1,1 +1,18 @@
-public class ParallelRace {}
+import java.util.List;
+
+public class ParallelRace {
+
+    public static int findMin(List<Integer> numbers) {
+        return numbers.stream().min(Integer::compare).orElse(0);
+    }
+
+    public static long countElements(List<Integer> numbers) {
+        return numbers.stream().count();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> sample = List.of(5, 12, 3, 20, 8);
+        System.out.println("Min value: " + findMin(sample));
+        System.out.println("Total elements: " + countElements(sample));
+    }
+}
