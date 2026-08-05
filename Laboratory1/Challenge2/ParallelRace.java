70a725742b2b14ccf447138aa8219acaf9eeb469
Daniel Santiago Ibañez Gonzalez <daniel.ibanez-g@mail.escuelaing.edu.co>
2026-08-04 21:55:19 -0500

    feat: lane one completed

--- Laboratory1/Challenge2/ParallelRace.java
+++ Laboratory1/Challenge2/ParallelRace.java
@@ -1,8 +1,7 @@
 import java.util.List;
 
 public class ParallelRace {
-
-    // Lane One: Retorna el número más grande usando Streams/Lambdas
+    
     public static int findMax(List<Integer> numbers) {
         return numbers.stream().max(Integer::compare).orElse(0);
     }
