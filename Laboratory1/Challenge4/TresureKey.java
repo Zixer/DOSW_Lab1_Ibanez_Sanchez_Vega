1e1ae779d8538d82a17172e55fc9a98aa2646450
Yazid Alejandro Sánchez Sánchez <yazid.sanchez-s@mail.escuelaing.edu.co>
2026-08-05 01:01:47 -0500

    Hashtable TreasureKey

---
+++ Laboratory1/Challenge4/TresureKey.java
@@ -0,0 +1,17 @@
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TreasureMap {
+
+    public static Hashtable<String, Integer> createTreasureMap(
+            List<Map.Entry<String, Integer>> entries) {
+
+        return entries.stream().collect(Collectors.toMap(
+                entry -> entry.getKey(),
+                entry -> entry.getValue(),
+                (firstValue, duplicateValue) -> firstValue,Hashtable::new
+        ));
+    }
+}
