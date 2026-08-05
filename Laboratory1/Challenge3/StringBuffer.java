3c2886d07ddb00b0e7da3bdde5725d33468cb379
Daniel Santiago Ibañez Gonzalez <daniel.ibanez-g@mail.escuelaing.edu.co>
2026-08-05 00:20:39 -0500

    feat: reverse

---
+++ Laboratory1/Challenge3/StringBuffer.java
@@ -0,0 +1,16 @@
+public class ReverseMessageMisEcho {
+
+    /**
+     * Receives a String and returns it reversed.
+     *
+     * @param message Message to reverse.
+     * @return Reversed message.
+     */
+    public static String reverseMessage(String message) {
+        StringBuffer buffer = new StringBuffer(message);
+        return buffer.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+    }
+}
